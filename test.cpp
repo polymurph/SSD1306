0f56dc943569a8d316378ee7d574eda3b9c1ea29
polymurph <eddyed.k@gmail.com>
2020-08-29 20:34:13 +0100

    working bash test file

--- test.cpp
+++ test.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 
 
+// for i2c functionality
+
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+
+
+#include <i2c/smbus.h>
+
+
 using namespace std;
+
+
+enum{
+	I2C_ADDR_SSD1306 = 0x3C
+};
+
 
 int main(void)
 {
 	cout << "SSD1306 test" << endl;	
 
 
-
+	
 
 	return 0;
 }
