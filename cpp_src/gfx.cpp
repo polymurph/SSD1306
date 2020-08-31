72336a858500f46d73817b829386f82f27a630a3
Edwin Koch <eddyed.k@gmail.com>
2020-08-31 13:31:36 +0200

    started work on basic gfx driver to test disp driver

---
+++ cpp_src/gfx.cpp
@@ -0,0 +1,85 @@
+#incldue "gfx.h"
+
+
+GFX::GFX(uint8_t n_pixle_row,
+		 uint8_t n_pixle_col,
+		 set_binary_pixle_t set_b_pix) :
+	n_pixle_row(n_pixle_row),
+	n_pixle_col(n_pixle_col),
+	set_bin_pixle()
+{
+	
+}
+
+void GFX::clear_display(bool foreground = false)
+{
+	uint8_t i;
+	uint8_t j;
+	
+	for(i = 0; i < n_pixle_row; i++) {
+		for(j = 0; j < n_pixle_col; j++) {
+			set_bin_pixle(i, j, foreground);
+		}
+	}
+}
+
+void GFX::draw_line(uint8_t x0,
+				    uint8_t y0,
+					uint8_t x1,
+					uint8_t y1,
+					uint8_t width = 1,
+					bool foreground = true)
+{
+	uint8_t x;
+	uint8_t y;
+	uint8_t x_start = ();
+	uint8_t x_end;
+	
+	// y = m * x + b
+	
+	// calculate slope m
+	float m = (y1 - y0) / (x1 - x0);
+	// calculate offset b
+	float b = (y0 > y1) ? y1 : y0;
+	
+	for(x = (x0 > x1) ? x1 : x0;
+	    x < (x0 > x1) ? x0 : x1;
+		x++) {
+		draw_kernel(x,
+					static_cast<uint8_t>(m * x + b),
+					width,
+					foreground);
+	}
+}
+
+void GFX::draw_kernel(uint8_t x,
+					  uint8_t y,
+					  uint8_t size = 1,
+					  bool foreground = true)
+{
+	if(size = 0) return;
+	if(size = 1) {
+		set_bin_pixle(x,y,foreground);
+		return;
+	}
+	
+	size -= 2;
+	
+	uint8_t k_size = kernel[size].size;
+	uint8_t i = 0;
+	uint8_t j = 0;
+	uint8_t offset = (k_size - 1) / 2; 
+	uint8_t x_start = x - offset;
+	uint8_t y_start = y - offset;
+	bool set = false;
+	
+	// drawing kernel centered on (x, y)
+	for(i = 0; i < k_size; i++) {
+		for(j = 0; j < k_size; j++) {
+			set = static_cast<bool>(kernel[size].pattern[i] >> j) ^ !foreground;
+			set_bin_pixle(x_start + i, y_start + j, set);
+		}
+	}
+}
+		
+
